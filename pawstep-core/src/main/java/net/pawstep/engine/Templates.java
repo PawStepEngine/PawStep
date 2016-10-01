261495dd5fed3babc27b8f33cfe633517e2e86ed
treyzania <treyzania@gmail.com>
2016-10-01 19:52:45 -0400

    Made the templater generate the objects instead of just copying them because apparently they aren't immutable.

--- pawstep-core/src/main/java/net/pawstep/engine/Templates.java
+++ pawstep-core/src/main/java/net/pawstep/engine/Templates.java
@@ -5,9 +5,16 @@ import org.lwjgl.util.vector.Vector3f;
 
 public class Templates {
 	
-	public static final Vector3f v3f_one = new Vector3f(1, 1, 1);
-	public static final Vector3f v3f_zero = new Vector3f(0, 0, 0);
+	public static final Vector3f v3f_one() {
+		return new Vector3f(1, 1, 1);
+	}
 	
-	public static final Quaternion quatIdentity = new Quaternion(0, 0, 0, 1);
+	public static final Vector3f v3f_zero() {
+		return new Vector3f(0, 0, 0);
+	}
+	
+	public static final Quaternion quatIdentity() {
+		return new Quaternion(0, 0, 0, 1);
+	}
 	
 }
